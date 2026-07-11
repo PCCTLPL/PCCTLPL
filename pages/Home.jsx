ccab7ba10cd4e8050280817194657ae210451e09
PCCTLPL <85776049+PCCTLPL@users.noreply.github.com>
2026-07-12 02:43:53 +0530

    Add modal functionality to Hero component

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,12 +1,16 @@
+import React, { useState } from 'react';
 import Hero from '../components/Sections/Hero';
+import QuoteModal from '../components/Common/QuoteModal';
 import FloatingButtons from '../components/Layout/FloatingButtons';
 
 const Home = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
     <div>
-      <Hero />
+      <Hero onQuoteClick={() => setIsModalOpen(true)} />
+      <QuoteModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
       <FloatingButtons />
-      {/* Baaki sections yahan aayenge */}
     </div>
   );
 };
