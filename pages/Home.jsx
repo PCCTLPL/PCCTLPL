68bff41959901a0f72321d5c9751572142f3d928
PCCTLPL <85776049+PCCTLPL@users.noreply.github.com>
2026-07-12 02:37:58 +0530

    Add Home component with Hero and FloatingButtons

---
+++ pages/Home.jsx
@@ -0,0 +1,14 @@
+import Hero from '../components/Sections/Hero';
+import FloatingButtons from '../components/Layout/FloatingButtons';
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <FloatingButtons />
+      {/* Baaki sections yahan aayenge */}
+    </div>
+  );
+};
+
+export default Home;
